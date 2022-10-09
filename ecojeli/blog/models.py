3fc35ae2d9cfac50237e23e8dc71502e25e288fe
pokemonguy1 <zadrot570@gmail.com>
2022-10-09 22:57:28 +0600

    front-end time

--- ecojeli/blog/models.py
+++ ecojeli/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.core.blocks import RichTextBlock
 from wagtail.images.blocks import ImageChooserBlock
@@ -20,6 +21,14 @@ class BlogIndexPage(Page):
 class BlogPage(Page):
 
     date = models.DateField("Дата публикации")
+    bgimg = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="Задний фон"
+    )
     intro = models.CharField(max_length=250, blank=True, null=True, verbose_name="Подзаголовок")
     body = RichTextField(blank=True, verbose_name="Содержание")
     author = models.CharField(max_length=250, blank=True, null=True, verbose_name="Автор")
@@ -31,6 +40,7 @@ class BlogPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
+        ImageChooserPanel('bgimg'),
         FieldPanel('intro'),
         FieldPanel('body'),
         FieldPanel('author'),
